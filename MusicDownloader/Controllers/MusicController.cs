f50ab8331fda1d072679e4272353c79d5a4d35ad
Pedro Valladares <pedrohvalladares@hotmail.com>
2024-06-22 20:54:27 -0300

    add api video download function

---
+++ MusicDownloader/Controllers/MusicController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicDownloader.Services.Downloader;
+using MusicDownloader.Services.IOManagement;
+
+namespace MusicDownloader.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase {
+
+        private readonly Downloader downloader;
+        private readonly ILogger logger;
+        private readonly FileManager fileManager;
+
+        public MusicController(Downloader downloader, FileManager fileManager) {
+            this.downloader = downloader;
+            this.fileManager = fileManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> DownloadVideo(string url) {
+            var videoInfo = await downloader.GetVideo(url);
+
+            if (videoInfo == null) {
+                return NoContent();
+            }
+
+            fileManager.Save(videoInfo.Title, "mp4", videoInfo.Data);
+
+            return Ok();
+        }
+    }
+}
