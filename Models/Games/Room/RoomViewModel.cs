358902086d89236584696a70f71698193be9a78b
Pedro Valladares <pedrohvalladares@hotmail.com>
2024-06-27 23:58:29 -0300

    moved models for a library project

---
+++ Models/Games/Room/RoomViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Games.Room {
+    public class RoomViewModel {
+        public List<RoomModel> Rooms { get; set; }
+    }
+}
