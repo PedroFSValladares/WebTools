9db1b24ca8894a60b64b1c660e5c2808aa61def0
Pedro Valladares <pedrohvalladares@hotmail.com>
2024-06-30 16:03:50 -0300

    adding missing model

---
+++ WebTools/Models/UserSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Reflection;
+
+namespace WebTools.Models
+{
+    public class UserSettings{
+        public string youtubeVideoDownloadUrl { get; set; }
+        public string defaultMediaDownloadFolderName { get; set; }
+        public string userListFolderName { get; set; }
+        
+        public Dictionary<string, string> ToDictionary() {
+            var dictionary = new Dictionary<string, string>();
+            var keyValueProperties = GetType().GetProperties().Select<PropertyInfo, (string key, string value)>(x => {
+                return (x.Name, x.GetValue(this).ToString());
+            });
+            foreach (var item in keyValueProperties){
+                dictionary.Add(item.key, item.value);
+            }
+            return dictionary;
+        }
+        
+    }
+}
